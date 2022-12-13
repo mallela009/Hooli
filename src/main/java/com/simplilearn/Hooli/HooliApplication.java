28e997f82512274cbd2249ac6f8d3bb9efb52e69
mallela009 <mallelaarjuna009@gmail.com>
2022-12-13 14:24:51 +0530

    first commit

---
+++ src/main/java/com/simplilearn/Hooli/HooliApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Hooli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HooliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/Hooli/HooliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Hooli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
